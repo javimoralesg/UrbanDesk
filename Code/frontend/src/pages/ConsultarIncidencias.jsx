f36f0b260e6d77c092de85711293fb1d8d9810c4
javimoralesg <javimorgalis@gmail.com>
2026-03-19 19:42:26 +0100

    update consultarIncidencias page

--- Code/frontend/src/pages/ConsultarIncidencias.jsx
+++ Code/frontend/src/pages/ConsultarIncidencias.jsx
@@ -1,16 +1,19 @@
 import Hero from "../assets/components/Hero";
 import Sidebar from "../assets/components/Sidebar";
 import MapLocate from '../assets/components/MapLocate';
+import "../assets/css/RegistrarIncidencia.css"; 
 
 export default function ConsultarIncidencias() {
   return (
     <>
       <Hero />
-      <Sidebar />
-        <h1>Consultar Incidencias</h1>
-        
-            <MapLocate />
-        
+      <main className="registrar-incidencia__layout">
+        <Sidebar className="registrar-incidencia__sidebar" />
+        <div className="registrar-incidencia__content">
+            <h1>Consultar Incidencias</h1>
+            <MapLocate width="100%" /> 
+        </div>
+      </main>
     </>
   );
 }
