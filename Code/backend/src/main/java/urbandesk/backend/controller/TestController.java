d17d22231fa28c7170f43275fb82c2a2523fa61d
AlejandroBarajas <alejandro.barajas.carracedo@alumnos.upm.es>
2026-04-01 22:23:37 +0200

    add TestController

---
+++ Code/backend/src/main/java/urbandesk/backend/controller/TestController.java
@@ -0,0 +1,27 @@
+package urbandesk.backend.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import urbandesk.backend.service.MailService;
+
+
+@RestController
+@RequestMapping("/test")
+@RequiredArgsConstructor
+public class TestController {
+
+    private final MailService mailService;
+
+    @GetMapping("/email")
+    public ResponseEntity<String> testEmail(@RequestParam String destinatario,
+                                             @RequestParam String nombre,
+                                             @RequestParam String email) {
+        mailService.enviarBienvenida(destinatario, nombre, email);
+        return ResponseEntity.ok("Email enviado correctamente a: " + destinatario);
+    }
+}
