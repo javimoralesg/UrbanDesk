0bd309494b0e2f814e174c0151c18d21d61dec1e
javimoralesg <javimorgalis@gmail.com>
2026-04-02 01:06:58 +0200

    add validation of incidence description

--- Code/frontend/src/pages/RegistrarIncidencia.jsx
+++ Code/frontend/src/pages/RegistrarIncidencia.jsx
@@ -46,6 +46,18 @@ export default function RegistrarIncidencia() {
             setIsSubmitting(true);
             setFeedback({ error: '', success: '' });
 
+            const response = await api.validateDescription(descripcion);
+            const parsedResponse = JSON.parse(response);
+            if (parsedResponse.valid !== true || parsedResponse.valid !== "true") {
+            
+                setFeedback({ error:  parsedResponse.reason || 'La descripción parece no ser válida para una incidencia urbana. Por favor, revisa y corrige la descripción.' , success: '' });
+                setTimeout(() => {
+                    setFeedback({ error: '', success: '' });
+                }, 4000);
+                setIsSubmitting(false);
+                return;
+            }
+
             await api.crearIncidencia({
                 direccion: address || 'Ubicación sin dirección textual',
                 latitud,
