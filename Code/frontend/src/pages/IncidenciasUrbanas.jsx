6e6753134b83c4402de8259bf401256ec2bc1f00
David Hernández Sola <davidhernandezsolaa@gmail.com>
2026-04-02 14:58:25 +0200

    habia error en una url

--- Code/frontend/src/pages/IncidenciasUrbanas.jsx
+++ Code/frontend/src/pages/IncidenciasUrbanas.jsx
@@ -16,7 +16,7 @@ const opcionesGlobales = {
         { text: "Portada", link: "/incidencias-urbanas" },
         { text: "Cerrar sesión", link: "/incidencias-urbanas/logout" },
         { text: "Editar perfil", link: "/incidencias-urbanas/editar-perfil " },
-        { text: "Consultar incidencias", link: "/incidencias-urbanas/consultar-incidencias" },
+        { text: "Consultar incidencias", link: "/incidencias-urbanas/mis-incidencias" },
         { text: "Registrar incidencia", link: "/incidencias-urbanas/registrar-incidencia" },
     ],
     Operador: [
